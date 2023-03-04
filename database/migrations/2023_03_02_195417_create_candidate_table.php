f089b17d5a3a010ff4958aa6dba44e29f99bd767
movix86 <movix86@gmail.com>
2023-03-04 14:53:16 -0500

    se removio un atributo no deseado de migration candidate

--- database/migrations/2023_03_02_195417_create_candidate_table.php
+++ database/migrations/2023_03_02_195417_create_candidate_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             //$table->string('email')->unique();
             //$table->timestamp('email_verified_at')->nullable();
             $table->integer('created_by');
-            $table->rememberToken();
+            //$table->rememberToken();
             $table->timestamps();
         });
     }
